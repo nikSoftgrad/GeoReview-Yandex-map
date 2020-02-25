967968cb8cd4ae0b5b906c3bfa47c48429b03504
nikolay_k <nik_k@softgrad.com>
2020-02-25 12:53:23 +0300

    end

--- src/scripts/model.js
+++ src/scripts/model.js
@@ -1,46 +1,150 @@
 
-export class Model {
+// export class Model {
+//     constructor(){
+//         this.map = null;
+//         this.marker = null;
+//         this.store = [];
+//     }
+//     clear(){
+//         this.marker = null;
+//         this.store = [];
+//     }
+//     initMap(){
+//         this.map = new ymaps.Map('map', {
+//             center: [47.258827504336296,39.71120850000003],
+//             controls: [],
+//             zoom: 15
+//         });
+//     }
+//     async createPlacemarkByCoords(coords){
+//         const response = await ymaps.geocode(coords);
+//         const address = response.geoObjects.get(0).getAddressLine();
+//         const placemark = this.createPlacemark(coords, { address });
+//         this.marker = placemark;
+
+//         return placemark;
+//     }
+//     createPlacemark(coords,properties){
+//         const placemark = new ymaps.Placemark(coords,{
+//             coords: coords,
+//             myId: properties.id || Date.now(),
+//             myAddress: properties.address,
+//             myReview: properties.reviewList || []
+//         },{
+//             preset: 'islands#icon'
+//         });
+
+//         return placemark;
+//     }
+//     addLocalStorage(review){
+//         console.log('review', review);
+//         if(!this.store.length){
+//             this.store.push(review);
+//         }else{
+//             if(this.store.filter(item => item.id === review.id).length){
+//                 this.store.filter(item => item.id === review.id)[0].reviewList = review.reviewList;
+//             }else{
+//                 this.store.push(review);
+//             }
+//         }
+//         console.log('this.store', this.store);
+
+//         localStorage.setItem('geoReview', JSON.stringify(this.store));
+//     }
+//     createPlacemarkFromLocalStorage(coords, properties){
+//         const placemark = this.createPlacemark(coords, properties);
+
+//         return placemark;
+//     }
+// }
+
+export class Model{
     constructor(){
+        this.cluster = null;
         this.map = null;
         this.marker = null;
         this.store = [];
     }
+    clear(){
+        this.cluster.removeAll();
+        this.marker = null;
+        this.store = [];
+    }
     initMap(){
-        this.map = new ymaps.Map('map', {
+        this.map = new ymaps.Map('map',{
             center: [47.258827504336296,39.71120850000003],
-            controls: [],
-            zoom: 15
+            zoom: 15,
+            controls: []
         });
     }
-    async createPlacemarkByCoords(coords){
+    createCluster(layout){
+        const clusterer = new ymaps.Clusterer({
+            preset: 'islands#invertedVioletClusterIcons',
+            clusterDisableClickZoom: true,
+            clusterBalloonContentLayout: 'cluster#balloonCarousel',
+            clusterBalloonItemContentLayout: layout,
+            openBalloonOnClick: true
+        });
+
+        this.cluster = clusterer;
+    }
+    async createMarkByCoords(coords){
         const response = await ymaps.geocode(coords);
         const address = response.geoObjects.get(0).getAddressLine();
-        const placemark = this.createPlacemark(coords, { address });
-        this.marker = placemark;
 
-        return placemark;
+        const mark = this.createPlacemark(coords, { address });
+        this.marker = mark;
+
+        return mark;
+
     }
-    createPlacemark(coords,properties){
-        const placemark = new ymaps.Placemark(coords,{
-            coords: coords,
+    createPlacemark(coords, properties){
+        const mark = new ymaps.Placemark(coords, {
+            myCoords: coords,
             myId: properties.id || Date.now(),
             myAddress: properties.address,
-            myReview: properties.reviewList || []
+            myReviews: properties.reviewList || []
         },{
-            preset: 'islands#icon'
+            preset: 'islands#violetDotIcon'
         });
 
-        return placemark;
+        return mark;
     }
-    addLocalStorage(review){
-        if(!this.store.length){
-            this.store.push(review);
-        }else{
+    addReview(review){
+        const obj = {
+            date: new Date().toLocaleString('ru', {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric'
+            })
+        };
 
-
-            this.store[0].reviewList = review.reviewList;
+        const placemarkReview = this.marker.properties.get('myReviews');
+        
+        review.forEach(input => {
+            obj[input.name] = input.value;
+            input.value = null;
+        });
+        
+        placemarkReview.push(obj);
+        
+        if(!this.findMark(item => item.marker)){
+            this.cluster.add(this.marker);
         }
+    }
+    findMark(f){
+        const placemarks = this.cluster.getGeoObjects();
+        const mark = placemarks.find(f);
 
-        // localStorage.setItem('geoReview', JSON.stringify(this.store));
+        return mark;
+    }
+    createMarkByLocalStorage(coords, properties){
+        const placemark = this.createPlacemark(coords, properties);
+        this.cluster.add(placemark);
+
+        return placemark;
     }
 }
--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,15 +1,85 @@
 
+// export class View{
+//     constructor(){}
+//     render(template, data){
+//         template = template + 'Template';
+
+//         const templateSource = document.querySelector(`#${template}`).innerHTML;
+//         const renderFn = Handlebars.compile(templateSource);
+
+//         return renderFn(data);
+//     }
+//     positionStyleModal(container, coords){
+//         const containerSize = {
+//             width : parseInt(getComputedStyle(container).width),
+//             height : parseInt(getComputedStyle(container).height)
+//         };
+//         const domSize ={
+//             width : document.body.clientWidth,
+//             height : document.body.clientHeight
+//         };
+
+//         if((containerSize.width + coords.X) > domSize.width && (containerSize.height + coords.Y) > domSize.height ){
+//             container.style.bottom = 0 + 'px';
+//             container.style.left = 0 + 'px';
+//         }
+//         else if((containerSize.width + coords.X) > domSize.width){
+//             container.style.bottom = coords.Y + 'px';
+//             container.style.right = 0 + 'px';
+//         }
+//         else if((containerSize.height + coords.Y) > domSize.height){
+//             container.style.bottom = 0 + 'px';
+//             container.style.left = coords.X + 'px';
+//         }
+//         else{
+//             container.style.top = coords.Y + 'px';
+//             container.style.left = coords.X + 'px';
+//         }
+
+//         return container;
+//     }
+//     removeStyleModal(container){
+//         container.style.top = '';
+//         container.style.bottom = '';
+//         container.style.left = '';
+//         container.style.right = '';
+
+//         return container;
+//     }
+// }
+
+const ymaps = window.ymaps;
 export class View{
-    constructor(){}
+    constructor(){
+        this.layouts = {
+            carousel: '<a href="#" class = "baloon__address">{{properties.myAddress}}</a>' +
+                        '<ul class = "baloon__reviews-list">' +
+                            '{% for review in properties.myReviews %}' +
+                            '<li class = "baloon__review-item">' +
+                                '<h2 class = "baloon__review-place">{{review.place}}</h2>' +
+                                '<div class ="baloon__review-alignment">' +
+                                    '<div class = "baloon__review-text">{{review.reviewText}}</div>' +
+                                    '<div class = "baloon__review-date">{{review.date}}</div>' +
+                                '</div>' +
+                            '</li>' +
+                            '{% endfor %}' +
+                        '</ul>'
+        };
+    }
+    makeLayout(layout){
+        const innerLayout = ymaps.templateLayoutFactory.createClass(this.layouts[layout]);
+
+        return innerLayout;
+    }
     render(template, data){
         template = template + 'Template';
 
-        const templateSource = document.querySelector(`#${template}`).innerHTML;
+        const templateSource = document.getElementById(template).innerHTML;
         const renderFn = Handlebars.compile(templateSource);
 
         return renderFn(data);
     }
-    positionStyleModal(container, coords){
+    positionModal(container, coords){
         const containerSize = {
             width : parseInt(getComputedStyle(container).width),
             height : parseInt(getComputedStyle(container).height)
@@ -43,7 +113,5 @@ export class View{
         container.style.bottom = '';
         container.style.left = '';
         container.style.right = '';
-
-        return container;
     }
 }
